54b4acef2eb2fbb20246cf3dc943bc142a427605
leticiacarvalho04 <leticiahelena.oliveira@outlook.com>
2025-06-08 00:58:28 -0300

    refatorando testes da classe LgnSpringApiApplicationTests para melhorar a legibilidade e a estrutura

--- src/test/java/br/edu/fatecsjc/lgnspringapi/LgnSpringApiApplicationTests.java
+++ src/test/java/br/edu/fatecsjc/lgnspringapi/LgnSpringApiApplicationTests.java
@@ -2,21 +2,25 @@ package br.edu.fatecsjc.lgnspringapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class LgnSpringApiApplicationTests {
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext, "Application context should load");
     }
 
     @Test
-    void testMainMethod() {
-        String[] args = {};
-        LgnSpringApiApplication.main(args);
-    }
-
-    @Test
-    void testNoArgsConstructor() {
-        new LgnSpringApiApplication();
+    void mainMethodRunsWithoutErrors() {
+        assertDoesNotThrow(() -> LgnSpringApiApplication.main(new String[] {}));
     }
 }
